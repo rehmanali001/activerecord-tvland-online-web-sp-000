533f5713e172134ce0ebdc5f0fd01ef560663f84
Rehman Ali <rehmanali001@gmail.com>
2019-09-06 20:44:21 +0000

    Automatically backed up by Learn

--- db/migrate/007_add_day_genre_season_to_shows.rb
+++ db/migrate/007_add_day_genre_season_to_shows.rb
@@ -1,5 +1,7 @@
-class AddCatchphraseToCharacters < ActiveRecord::Migration[5.1]
+class AddDayGenreSeasonToShows < ActiveRecord::Migration[5.1]
   def change 
-    
+    add column :shows, :genre, :string
+     add column :shows, :genre, :string
+    add column :shows, :genre, :string
   end 
 end 
