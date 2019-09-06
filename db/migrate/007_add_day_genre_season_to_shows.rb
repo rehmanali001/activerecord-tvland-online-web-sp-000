ebb790df3e04a3ac8f974fb7b115d209e4e2a68e
Rehman Ali <rehmanali001@gmail.com>
2019-09-06 20:45:59 +0000

    Automatically backed up by Learn

--- db/migrate/007_add_day_genre_season_to_shows.rb
+++ db/migrate/007_add_day_genre_season_to_shows.rb
@@ -1,7 +1,7 @@
 class AddDayGenreSeasonToShows < ActiveRecord::Migration[5.1]
   def change 
-    add column :shows, :day, :string,
-    add column :shows, :genre, :string,
-    add column :shows, :season, :string
+    add_column :shows, :day, :string,
+    add_column :shows, :genre, :string,
+    add_column :shows, :season, :string
   end 
 end 
