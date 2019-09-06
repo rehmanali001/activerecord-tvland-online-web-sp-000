cf867dd1168d9215e1ef4b481f331390a327dda2
Rehman Ali <rehmanali001@gmail.com>
2019-09-06 20:21:35 +0000

    Automatically backed up by Learn

--- db/migrate/006_add_catchphrase_to_characters.rb
+++ db/migrate/006_add_catchphrase_to_characters.rb
@@ -1,3 +1,3 @@
-class AddCatchphraseToCharacter < ActiveRecord::Migration[5.1]
+class AddCatchphraseToCharacters < ActiveRecord::Migration[5.1]
   add_column :characters, :catchphrase, :string
 end 
